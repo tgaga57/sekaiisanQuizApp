5d56cff593cceccefac10c60ea42982574101db3
志賀大河 <shigataiga@shigataiganoMacBook-Pro.local>
2019-08-13 16:42:58 +0900

    first commit

---
+++ sekaiisannQuizApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sekaiisannQuizApp
+//
+//  Created by 志賀大河 on 2019/08/13.
+//  Copyright © 2019 Taigashiga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
